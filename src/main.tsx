84a9017c1fc7646e89be17b06e255c24f725ea57
IKLANLO <ilandaberea@yahoo.es>
2024-05-31 18:41:25 +0000

    comentario menor

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// con ! le decimos a react que el elemento root no va a ser null
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
